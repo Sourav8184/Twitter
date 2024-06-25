c5c4f421e740738e861161d6ba2ebf4ec073f197
Sourav <ksourav662@gmail.com>
2024-06-26 00:53:58 +0530

    Complete signup and login page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+// React and React Router Dom:
+import { Routes, Route } from "react-router-dom";
+
+// Components:
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+
+// App Component:
 function App() {
-  return <h1>App</h1>;
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
